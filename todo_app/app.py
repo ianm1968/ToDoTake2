d98b218f8364f8ae8f50fbc858ff076ad47e5cd4
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-05-01 19:20:25 +0100

    WIP - trello moves first card, no error handling

--- todo_app/app.py
+++ todo_app/app.py
@@ -11,8 +11,24 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    trello.get_boards_from_my_id()
-    print(trello.get_boards_from_me())
+    my_boards = trello.get_boards_from_me()
+    # print(my_boards)
+    my_first_board=trello.get_board_from_board_id(my_boards[0]['id'])
+    # print(my_first_board)
+    my_first_board_lists=trello.get_lists_from_board_id(my_first_board['id'])
+    # print(my_first_board_lists)
+    my_first_board_first_list_cards=trello.get_cards_in_list_from_list_id(my_first_board_lists[0]['id'])
+    # print(my_first_board_first_list_cards)
+    my_first_board_first_list_first_card=trello.get_card_from_card_id(my_first_board_first_list_cards[0]['id'])
+    # print(my_first_board_first_list_first_card)
+    
+    #move first card to last list
+    num_lists = len(my_first_board_lists)
+    last_list_id = my_first_board_lists[num_lists-1]['id']
+    print(last_list_id)
+    trello.move_card_to_list(my_first_board_first_list_first_card['id'],last_list_id)
+    trello.
+    
     sorted_items = sorted(get_items(), key=lambda item: item.get('status'), reverse=True)
     return render_template("index.html", to_do_list=sorted_items)
     
--- todo_app/trello.py
+++ todo_app/trello.py
@@ -13,67 +13,68 @@ def get_boards_from_me():
 
 def get_boards_from_my_id():
     url = f"{HOME}/members/{os.getenv('MEMBERID')}/boards"
-    print(url)
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())
+    return response.json()
     # return render_template('index.html', landing_image=content['hdurl'])
 
 # get_board_from_board_id
 # GET /1/boards/{id}
 def get_board_from_board_id(board_id):
     url = f"{HOME}/boards/{board_id}"
-    print(url)
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())
+    return response.json()
 
 
 # get_lists_from_board_id
 # GET /1/boards/{id}/lists
 def get_lists_from_board_id(board_id):
     url = f"{HOME}/boards/{board_id}/lists"
-    print(url)
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())
+    return response.json()
 
 
 # get_list_from_list_id
 # GET /1/lists/{id}
 def get_list_from_list_id(list_id):
     url = f"{HOME}/lists/{list_id}"
-    print(url)
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())
+    return response.json()
     
 # get_cards_in_list_from_list_id
 # GET /1/lists/{id}/cards
 def get_cards_in_list_from_list_id(list_id):
     url = f"{HOME}/lists/{list_id}/cards"
-    print(url)
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())
+    return response.json()
     
 # get_card_from_card_id
 # GET /1/cards/{id}
 def get_card_from_card_id(card_id):
     url = f"{HOME}/cards/{card_id}"
-    print(url)
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())
+    return response.json()
     
 # update_card_by_card_id
 # PUT /1/cards/{id}
-def update_card_by_card_id(card_id):
+# def update_card_by_card_id(card_id):
+#     url = f"{HOME}/cards/{card_id}"
+#     response = requests.put(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
+#     return response.json()    
+
+# move_card_to_list
+# PUT /1/cards/{cardID}?idList={listID}
+def move_card_to_list(card_id, list_id):
     url = f"{HOME}/cards/{card_id}"
-    print(url)
-    response = requests.put(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())    
+    response = requests.put(url,params={'idList':list_id,'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
+    print (response.status_code)
+    return response.status_code    
 
 # delete_card_by_card_id
 # DELETE /1/cards/{id}
-def delete_card_by_card_id(card_id):
-    url = f"{HOME}/cards/{card_id}"
-    print(url)
-    response = requests.delete(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
-    print(response.json())    
+# def delete_card_by_card_id(card_id):
+    # url = f"{HOME}/cards/{card_id}"
+    # print(url)
+    # response = requests.delete(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
+    # return response.json()    
 
 
