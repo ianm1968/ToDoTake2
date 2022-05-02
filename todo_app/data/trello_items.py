e112a418afe18750b6a3015997c80b5a7f7bf2f6
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-05-02 17:02:01 +0100

    wip implement session

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,8 +1,56 @@
 import requests
 import os
-# import json
+import json
 
 HOME = 'https://api.trello.com/1'
+
+
+_DEFAULT_ITEMS = [
+    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+]
+
+
+def parse(raw):
+    # get list
+    # get all cards in list
+    # parse
+    # for each 
+    return cooked
+
+def get_items():
+        # my_boards = get_boards_from_my_id()
+        # my_first_board = get_board_from_board_id(my_boards[0]['id'])
+        # my_boards = get_boards_from_me()
+        # print(my_boards)
+        # my_board=get_board_from_board_id(os.getenv('BOARDID'))
+        # print(my_board)
+        my_board_lists = get_lists_from_board_id(os.getenv('BOARDID'))
+        # print(my_board_lists)
+        # open_cards=get_cards_in_list_from_list_id(my_board_lists[0]['id'])
+        open_cards = get_open_cards_in_lists_from_board_id(os.getenv('BOARDID'));
+        print(open_cards)
+        return open_cards #DEFAULT_ITEMS.copy()
+        """
+        Fetches all saved items from the session.
+
+        Returns:
+            list: The list of saved items.
+        """
+        # my_boards = get_boards_from_me()
+        # print(my_boards)
+        # my_first_board=trello.get_board_from_board_id(my_boards[0]['id'])
+        # # print(my_first_board)
+
+    
+def add_item(title):
+        return True
+def get_item(id):
+        return True
+def save_item(item):
+        return True
+def delete_item(id):
+        return True
 
 def get_boards_from_me():
     url = f'{HOME}/members/me/boards'
@@ -37,14 +85,23 @@ def get_list_from_list_id(list_id):
     response = requests.get(url,params={'key':os.getenv('API_KEY'), 'token':os.getenv('TOKEN')})
     return response.json()
     
-# get_cards_in_list_from_list_id
-# GET /1/lists/{id}/cards
-def get_open_cards_in_list_from_list_id(list_id):
-    url = f"{HOME}/lists/{list_id}/cards"
+    
+def get_open_cards_in_lists_from_board_id(board_id):
+    url = f"{HOME}/boards/{board_id}/lists"
     response = requests.get(url,params={
     'key':os.getenv('API_KEY'), 
     'token':os.getenv('TOKEN'),
     'cards':'open'
+    })
+    return response.json()
+        
+# get_cards_in_list_from_list_id
+# GET /1/lists/{id}/cards
+def get_cards_in_list_from_list_id(list_id):
+    url = f"{HOME}/lists/{list_id}/cards"
+    response = requests.get(url,params={
+    'key':os.getenv('API_KEY'), 
+    'token':os.getenv('TOKEN')
     })
     return response.json()
     
