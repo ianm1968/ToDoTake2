fd0b6bd41833d01a79dd97146da314b86478535b
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-05-30 01:04:06 +0100

    Fixtures for lists in tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 # from todo_app.data.trello_items import Item
+import pytest
 from todo_app.models.view_model import ViewModel
 
 class Item:
@@ -7,37 +8,49 @@ class Item:
         self.status = status
         self.desc = desc
         self.due = due
-    
 
-def test_to_do_items():
-    # arrange
+@pytest.fixture
+def test_list_one_in_to_do():
     test_item = Item(name='fred',status='To Do')
     test_list=[]
     test_list.append(test_item)
-    test_model=ViewModel(test_list)
+    return test_list    
+
+@pytest.fixture
+def test_list_one_in_doing():
+    test_item = Item(name='fred',status='To Do')
+    test_list=[]
+    test_list.append(test_item)
+    return test_list    
+
+@pytest.fixture
+def test_list_one_in_done():
+    test_item = Item(name='fred',status='To Do')
+    test_list=[]
+    test_list.append(test_item)
+    return test_list    
+
+
+def test_to_do_items(test_list_one_in_to_do):
+    # arrange
+    test_model=ViewModel(test_list_one_in_to_do)
     # act
     test_to_do_items = test_model.to_do_items
     # assert
     assert len(test_to_do_items) == 1
 
-def test_doing_items():
+def test_doing_items(test_list_one_in_to_do):
     # arrange
-    test_item = Item(name='fred',status='Doing')
-    test_list=[]
-    test_list.append(test_item)
-    test_model=ViewModel(test_list)
+    test_model=ViewModel(test_list_one_in_to_do)
     # act
     test_to_do_items = test_model.doing_items
     # assert
     assert len(test_to_do_items) == 1
 
 
-def test_done_items():
+def test_done_items(test_list_one_in_to_do):
     # arrange
-    test_item = Item(name='fred',status='Done')
-    test_list=[]
-    test_list.append(test_item)
-    test_model=ViewModel(test_list)
+    test_model=ViewModel(test_list_one_in_to_do)
     # act
     test_to_do_items = test_model.done_items
     # assert
