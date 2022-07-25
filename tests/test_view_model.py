b5740c9222eaa56b980844963e1d21634a360145
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-07-25 16:21:22 +0100

    refactoring repetitive fixtures WIP

--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -7,20 +7,28 @@ from dotenv import load_dotenv, find_dotenv
 file_path = find_dotenv('.env.test')
 load_dotenv(file_path, override=True)
 
-@pytest.fixture
-def board_one_in_to_do():
-    test_list = [Item(id=0,name='fred',status='To Do')]
-    return test_list    
 
-@pytest.fixture
-def board_one_in_doing():
-    test_list = [Item(id=0,name='fred',status='Doing')]
-    return test_list    
+def build_items(num_items, item_name, date_last_activity, status):
+    this_list=[]
+    # this_list.append(Item(id=0,name='fred',status='To Do'))
+    for i in range(num_items):
+        this_list.append(Item(0,item_name, date_last_activity, status))
+    return this_list
 
-@pytest.fixture
-def board_one_in_done():
-    test_list = [Item(id=0,name='fred',status='Done')]
-    return test_list    
+# @pytest.fixture
+# def board_one_in_to_do():
+#     return build_items(1, 'fred','','To Do')
+#     # [Item(id=0,name='fred',status='To Do')]
+
+# @pytest.fixture
+# def board_one_in_doing():
+#     test_list = [Item(name='fred',status='Doing',id=0)]
+#     return test_list    
+
+# @pytest.fixture
+# def board_one_in_done():
+#     test_list = [Item(id=0,name='fred',status='Done')]
+#     return test_list    
 
 @pytest.fixture
 def board_done_three_today_one_yesterday():
@@ -96,14 +104,14 @@ def board_done_1today_2yesterday_3last_week_4tomorrow():
         test_list.append(test_item)
     return test_list    
 
-@pytest.fixture
-def board_one_in_each():
-    test_list=[
-        Item(id=0,name='Tom',status='To Do'),
-        Item(id=0,name='Dick',status='Doing'),
-        Item(id=0,name='Harry',status='Done')
-    ]
-    return test_list    
+# @pytest.fixture
+# def board_one_in_each():
+#     test_list=[
+#         Item(id=0,name='Tom',status='To Do'),
+#         Item(id=0,name='Dick',status='Doing'),
+#         Item(id=0,name='Harry',status='Done')
+#     ]
+#     return test_list    
 
 @pytest.fixture
 def board_one_hundred_in_each():
@@ -118,56 +126,79 @@ def board_one_hundred_in_each():
         i += 1
     return test_list    
 
+
+
+
+
 def test_no_items():
     test_model=ViewModel([])
     assert len(test_model.to_do_items) == 0
     assert len(test_model.doing_items) == 0
     assert len(test_model.done_items) == 0
 
-def test_one_in_to_do(board_one_in_to_do):
-    test_model=ViewModel(board_one_in_to_do)
+def test_one_in_to_do():
+    test_model=ViewModel(build_items(1, 'fred','','To Do'))
     assert len(test_model.to_do_items) == 1
     assert test_model.to_do_items[0].status == 'To Do'
     assert len(test_model.doing_items) == 0
     assert len(test_model.done_items) == 0
  
 
-def test_one_in_doing(board_one_in_doing):
-    test_model=ViewModel(board_one_in_doing)
+def test_one_in_doing():
+    test_model=ViewModel(build_items(1, 'fred','','Doing'))
     assert len(test_model.to_do_items) == 0
     assert len(test_model.doing_items) == 1
     assert test_model.doing_items[0].status == 'Doing'
     assert len(test_model.done_items) == 0
 
-def test_one_in_done(board_one_in_done):
-    test_model=ViewModel(board_one_in_done)
+def test_one_in_done():
+    test_model=ViewModel(build_items(1, 'fred','','Done'))
     assert len(test_model.to_do_items) == 0
     assert len(test_model.doing_items) == 0
     assert len(test_model.done_items) == 1
     assert test_model.done_items[0].status == 'Done'
 
-def test_one_in_each(board_one_in_each):
-    test_model=ViewModel(board_one_in_each)
+def test_one_in_each():
+    test_list = build_items(1, 'tom','','To Do')
+    test_list += build_items(1, 'dick','','Doing')
+    test_list += build_items(1, 'harry','','Done')
+    test_model=ViewModel(test_list)
+    # test_model=ViewModel(test_items)
+    assert len(test_model.done_items) == 1
     assert len(test_model.to_do_items) == 1
     assert len(test_model.doing_items) == 1
-    assert len(test_model.done_items) == 1
     assert test_model.to_do_items[0].status == 'To Do'
     assert test_model.doing_items[0].status == 'Doing'
     assert test_model.done_items[0].status == 'Done'
     
 
-def test_one_hundred_in_each(board_one_hundred_in_each):
-    test_model=ViewModel(board_one_hundred_in_each)
+def test_one_hundred_in_each():
+    test_list = build_items(100, 'tom','','To Do')
+    test_list += build_items(100, 'dick','','Doing')
+    test_list += build_items(100, 'harry','','Done')
+    test_model=ViewModel(test_list)
     assert len(test_model.to_do_items) == 100
     assert len(test_model.doing_items) == 100
     assert len(test_model.done_items) == 100
 
-def test_should_show_three_today_one_yesterday(board_done_three_today_one_yesterday):
-    test_items=ViewModel(board_done_three_today_one_yesterday)
+def test_should_show_three_today_one_yesterday():
+    today=datetime.now()
+    yesterday=today-timedelta(days=1)
+    today_str = datetime.strftime(today,'%Y-%m-%dT%H:%M:%S.%fZ')
+    yesterday_str = datetime.strftime(yesterday,'%Y-%m-%dT%H:%M:%S.%fZ')
+    test_list = build_items(3, 'Dick',today_str,'Done')
+    test_list += build_items(1, 'Harry',yesterday_str,'Done')
+    test_items=ViewModel(test_list)
     assert test_items.should_show_all_done_items == True
 
-def test_shouldnt_show_three_today_two_yesterday(board_done_three_today_two_yesterday):
-    test_items=ViewModel(board_done_three_today_two_yesterday)
+def test_shouldnt_show_three_today_two_yesterday():
+    today=datetime.now()
+    yesterday=today-timedelta(days=1)
+    today_str = datetime.strftime(today,'%Y-%m-%dT%H:%M:%S.%fZ')
+    yesterday_str = datetime.strftime(yesterday,'%Y-%m-%dT%H:%M:%S.%fZ')
+    test_list = build_items(3, 'Dick',today_str,'Done')
+    test_list += build_items(2, 'Harry',yesterday_str,'Done')
+    test_items=ViewModel(test_list)
     assert test_items.should_show_all_done_items == False
 
 def test_shouldnt_show_four_today_one_yesterday(board_done_four_today_one_yesterday):
